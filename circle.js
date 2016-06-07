8d2fd7634037d61f86c47bc54a92aa3ecdda7fdc
zhouzhaoxu <zhouzhaoxu@yahoo.com>
2016-06-06 23:17:45 -0700

    Update circle.js

--- circle.js
+++ circle.js
@@ -106,137 +106,135 @@ Circle.prototype.collideBottom = function () {
 
 Circle.prototype.update = function () {
     Entity.prototype.update.call(this);
-   //console.log(this.game.clockTick);
-    if(!freeze) {
-	    this.x += this.velocity.x * this.game.clockTick;
-	    this.y += this.velocity.y * this.game.clockTick;
-	
-	    //left and right wall
-	    if (this.collideLeft() || this.collideRight()) {
-	        this.velocity.x = -this.velocity.x * friction;
-	        if (this.collideLeft()) this.x = this.radius;
-	        if (this.collideRight()) this.x = 800 - this.radius;
-	        this.x += this.velocity.x * this.game.clockTick;
-	        this.y += this.velocity.y * this.game.clockTick;
-	    }
-	
-	    //top and bottom wall
-	    if (this.collideTop() || this.collideBottom()) {
-	        this.velocity.y = -this.velocity.y * friction;
-	        if (this.collideTop()) this.y = this.radius;
-	        if (this.collideBottom()) this.y = 800 - this.radius;
-	        this.x += this.velocity.x * this.game.clockTick;
-	        this.y += this.velocity.y * this.game.clockTick;
-	    }
-	
-	    //other circle and base
-	    for (var i = 0; i < this.game.entities.length; i++) {
-	        var ent = this.game.entities[i];
-	        
-	        if(ent == this && !this.isAlive) {
-	        	this.game.entities[i].removeFromWorld = true;
-	            break;
-	        }
-	        if(ent instanceof Circle) {
-	            //same color
-	            if (ent !== this && this.collide(ent)) {
-	                if(this.color == ent.color) {
-	                    var temp = { x: this.velocity.x, y: this.velocity.y };
-	
-	                    var dist = distance(this, ent);
-	                    var delta = this.radius + ent.radius - dist;
-	                    var difX = (this.x - ent.x)/dist;
-	                    var difY = (this.y - ent.y)/dist;
-	
-	                    this.x += difX * delta / 2;
-	                    this.y += difY * delta / 2;
-	                    ent.x -= difX * delta / 2;
-	                    ent.y -= difY * delta / 2;
-	
-	                    this.velocity.x = ent.velocity.x * friction;
-	                    this.velocity.y = ent.velocity.y * friction;
-	                    ent.velocity.x = temp.x * friction;
-	                    ent.velocity.y = temp.y * friction;
-	                    this.x += this.velocity.x * this.game.clockTick;
-	                    this.y += this.velocity.y * this.game.clockTick;
-	                    ent.x += ent.velocity.x * this.game.clockTick;
-	                    ent.y += ent.velocity.y * this.game.clockTick;
-	                }
-	                
-	                if (this.color !== ent.color) {
-	                    this.radius -= 10;
-	                    ent.radius -= 10;
-	                    if(this.radius <= 0) {
-	                        this.isAlive = false;
-	                    }
-	                    if(ent.radius <= 0) {
-	                        ent.isALive = false;
-	                        this.game.entities[i].removeFromWorld = true;
-	                    }		            	
-	                }
-	            }
-	        } else {
-	             if (ent !== this && this.collideRect(ent)) {
-	                 if (this.color !== ent.color) {
-	                    this.isAlive = false;
-	                    ent.health -= this.radius / 10;
-	                }
-	             }
-	        }
-	        
-	
-	        //follow
-	        if(ent instanceof Circle) {
-	            if (ent != this && this.collide({ x: ent.x, y: ent.y, radius: this.visualRadius })) {
-	                var dist = distance(this, ent);
-	                if (this.color !== ent.color && dist > this.radius + ent.radius + 10) {
-	                    var difX = (ent.x - this.x)/dist;
-	                    var difY = (ent.y - this.y)/dist;
-	                    /*if(difX < 0) {
-	                        this.velocity.x *= -1;
-	                    }
-	                    if(difY < 0) {
-	                        this.velocity.y *= -1;
-	                    }*/
-	                    //this.velocity.x *= difX;
-	                    //this.velocity.y *= difY;
-	                    this.velocity.x += difX * acceleration / (dist*dist);
-	                    this.velocity.y += difY * acceleration / (dist * dist);
-	                    var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
-	                    if (speed > this.maxSpeed) {
-	                        var ratio = this.maxSpeed / speed;
-	                        this.velocity.x *= ratio;
-	                        this.velocity.y *= ratio;
-	                    }
-	                }
-	            }
-	        } else {
-	            if(ent != this && this.collideRect2({ x: ent.x, y: ent.y, side: ent.side}, this.visualRadius)) {
-	                var dist = distance(this, ent);
-	                if (this.color !== ent.color && dist > this.radius + ent.side + 10) {
-	                    var difX = (ent.x - this.x)/dist;
-	                    var difY = (ent.y - this.y)/dist;
-	                    this.velocity.x += difX * acceleration / (dist*dist);
-	                    this.velocity.y += difY * acceleration / (dist * dist);
-	                    var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
-	                    if (speed > this.maxSpeed) {
-	                        if(ratio < 0.3) {
-	                            ratio = 0.3;
-	                        }
-	                        var ratio = this.maxSpeed / speed;
-	                        this.velocity.x *= ratio;
-	                        this.velocity.y *= ratio;
-	                    }
-	                }
-	            }
-	        }
-	        
-	    }
-	
-	    //slow down due to friction
-	    this.velocity.x -= (1 - friction) * this.game.clockTick * this.velocity.x;
-	    this.velocity.y -= (1 - friction) * this.game.clockTick * this.velocity.y;
+    //console.log(this.game.clockTick);
+    this.x += this.velocity.x * this.game.clockTick;
+    this.y += this.velocity.y * this.game.clockTick;
+
+    //left and right wall
+    if (this.collideLeft() || this.collideRight()) {
+        this.velocity.x = -this.velocity.x * friction;
+        if (this.collideLeft()) this.x = this.radius;
+        if (this.collideRight()) this.x = 800 - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
     }
+
+    //top and bottom wall
+    if (this.collideTop() || this.collideBottom()) {
+        this.velocity.y = -this.velocity.y * friction;
+        if (this.collideTop()) this.y = this.radius;
+        if (this.collideBottom()) this.y = 800 - this.radius;
+        this.x += this.velocity.x * this.game.clockTick;
+        this.y += this.velocity.y * this.game.clockTick;
+    }
+
+    //other circle and base
+    for (var i = 0; i < this.game.entities.length; i++) {
+        var ent = this.game.entities[i];
+        
+        if(ent == this && !this.isAlive) {
+        	this.game.entities[i].removeFromWorld = true;
+            break;
+        }
+        if(ent instanceof Circle) {
+            //same color
+            if (ent !== this && this.collide(ent)) {
+                if(this.color == ent.color) {
+                    var temp = { x: this.velocity.x, y: this.velocity.y };
+
+                    var dist = distance(this, ent);
+                    var delta = this.radius + ent.radius - dist;
+                    var difX = (this.x - ent.x)/dist;
+                    var difY = (this.y - ent.y)/dist;
+
+                    this.x += difX * delta / 2;
+                    this.y += difY * delta / 2;
+                    ent.x -= difX * delta / 2;
+                    ent.y -= difY * delta / 2;
+
+                    this.velocity.x = ent.velocity.x * friction;
+                    this.velocity.y = ent.velocity.y * friction;
+                    ent.velocity.x = temp.x * friction;
+                    ent.velocity.y = temp.y * friction;
+                    this.x += this.velocity.x * this.game.clockTick;
+                    this.y += this.velocity.y * this.game.clockTick;
+                    ent.x += ent.velocity.x * this.game.clockTick;
+                    ent.y += ent.velocity.y * this.game.clockTick;
+                }
+                
+                if (this.color !== ent.color) {
+                    this.radius -= 10;
+                    ent.radius -= 10;
+                    if(this.radius <= 0) {
+                        this.isAlive = false;
+                    }
+                    if(ent.radius <= 0) {
+                        ent.isALive = false;
+                        this.game.entities[i].removeFromWorld = true;
+                    }		            	
+                }
+            }
+        } else {
+             if (ent !== this && this.collideRect(ent)) {
+                 if (this.color !== ent.color) {
+                    this.isAlive = false;
+                    ent.health -= this.radius / 10;
+                }
+             }
+        }
+        
+
+        //follow
+        if(ent instanceof Circle) {
+            if (ent != this && this.collide({ x: ent.x, y: ent.y, radius: this.visualRadius })) {
+                var dist = distance(this, ent);
+                if (this.color !== ent.color && dist > this.radius + ent.radius + 10) {
+                    var difX = (ent.x - this.x)/dist;
+                    var difY = (ent.y - this.y)/dist;
+                    /*if(difX < 0) {
+                        this.velocity.x *= -1;
+                    }
+                    if(difY < 0) {
+                        this.velocity.y *= -1;
+                    }*/
+                    //this.velocity.x *= difX;
+                    //this.velocity.y *= difY;
+                    this.velocity.x += difX * acceleration / (dist*dist);
+                    this.velocity.y += difY * acceleration / (dist * dist);
+                    var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
+                    if (speed > this.maxSpeed) {
+                        var ratio = this.maxSpeed / speed;
+                        this.velocity.x *= ratio;
+                        this.velocity.y *= ratio;
+                    }
+                }
+            }
+        } else {
+            if(ent != this && this.collideRect2({ x: ent.x, y: ent.y, side: ent.side}, this.visualRadius)) {
+                var dist = distance(this, ent);
+                if (this.color !== ent.color && dist > this.radius + ent.side + 10) {
+                    var difX = (ent.x - this.x)/dist;
+                    var difY = (ent.y - this.y)/dist;
+                    this.velocity.x += difX * acceleration / (dist*dist);
+                    this.velocity.y += difY * acceleration / (dist * dist);
+                    var speed = Math.sqrt(this.velocity.x*this.velocity.x + this.velocity.y*this.velocity.y);
+                    if (speed > this.maxSpeed) {
+                        if(ratio < 0.3) {
+                            ratio = 0.3;
+                        }
+                        var ratio = this.maxSpeed / speed;
+                        this.velocity.x *= ratio;
+                        this.velocity.y *= ratio;
+                    }
+                }
+            }
+        }
+        
+    }
+
+    //slow down due to friction
+    this.velocity.x -= (1 - friction) * this.game.clockTick * this.velocity.x;
+    this.velocity.y -= (1 - friction) * this.game.clockTick * this.velocity.y;
 };
 
 Circle.prototype.draw = function (ctx) {
